ca0fd58e22a915ed0a35815130f96c53c71a0df2
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-08-26 13:06:52 +0100

    ch: added sidebar push feature

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,3 +6,13 @@ window.onload = (event) => {
     let bsAlert = new bootstrap.Toast(pageAlert);
     bsAlert.show()
 }
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "18rem";
+    document.getElementById("main-body").style.marginLeft = "18rem";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidebar").style.width = "4.4rem";
+    document.getElementById("main-body").style.marginLeft= "4.4rem";
+  }
