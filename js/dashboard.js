cc8dc1ed39711916cdc7c8b4bfac0e05a3280460
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-08-26 21:32:36 +0100

    ch: added property import page

--- js/dashboard.js
+++ js/dashboard.js
@@ -10,9 +10,11 @@ window.onload = (event) => {
 function openNav() {
     document.getElementById("sidebar").style.width = "18rem";
     document.getElementById("main-body").style.marginLeft = "18rem";
+    document.getElementById("prop-nav").style.paddingLeft = "1rem";
   }
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "4.4rem";
-    document.getElementById("main-body").style.marginLeft= "4.4rem";
+    document.getElementById("main-body").style.marginLeft= "0";
+    document.getElementById("prop-nav").style.paddingLeft = "7rem";
   }
