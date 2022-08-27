69be3112d859ad3f876baabf4e0597512a892490
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-08-27 18:26:24 +0100

    ch: fixed property-import width bug

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,10 +11,12 @@ function openNav() {
     document.getElementById("sidebar").style.width = "18rem";
     document.getElementById("main-body").style.marginLeft = "18rem";
     document.getElementById("prop-nav").style.paddingLeft = "1rem";
+    document.getElementById("btn-black").style.width = "95%";
   }
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "4.4rem";
     document.getElementById("main-body").style.marginLeft= "0";
     document.getElementById("prop-nav").style.paddingLeft = "7rem";
+    document.getElementById("btn-black").style.width = "87%";
   }
