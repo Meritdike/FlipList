dfbe70ca0513b291be3f9fe3ad29fef4ae445652
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-08-27 23:02:28 +0100

    ch: fixed toast bug

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@
 // const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, option))
 
 window.onload = (event) => {
-    let pageAlert = document.querySelector('.toast');
+    let pageAlert = document.querySelector('.toast-d');
     let bsAlert = new bootstrap.Toast(pageAlert);
     bsAlert.show()
 }
