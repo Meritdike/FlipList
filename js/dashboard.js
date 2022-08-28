802eebde1a01ccda4a60890e0b4766c87777e19f
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-08-29 00:57:15 +0100

    bg: fixed nav bug

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,14 +11,18 @@ function openNav() {
     document.getElementById("sidebar").style.width = "18rem";
     document.getElementById("main-body").style.marginLeft = "18rem";
     document.getElementById("prop-nav").style.paddingLeft = "1rem";
+    document.getElementById("prop-import").style.marginTop = "0rem";
     document.getElementById("btn-black").style.width = "95%";
+    document.getElementById("fixed-top").style.position = "static";
   }
   
   function closeNav() {
     document.getElementById("sidebar").style.width = "4.4rem";
     document.getElementById("main-body").style.marginLeft= "0";
     document.getElementById("prop-nav").style.paddingLeft = "7rem";
+    document.getElementById("prop-import").style.marginTop = "4.7rem";
     document.getElementById("btn-black").style.width = "87%";
+    document.getElementById("fixed-top").style.position = "fixed";
   }
 
 
